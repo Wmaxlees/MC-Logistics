31ad03a24014afc7981fb098e7eee4202131f43f
wmaxlees <max.lees@gmail.com>
2024-09-10 22:54:43 -0600

    Fluid warehouses (and any other buildings) can now use any fluid tanks

---
+++ src/main/java/com/wmaxlees/gregcolonies/core/colony/buildings/moduleviews/InventoryUserModuleView.java
@@ -0,0 +1,52 @@
+package com.wmaxlees.gregcolonies.core.colony.buildings.moduleviews;
+
+import com.ldtteam.blockui.views.BOWindow;
+import com.minecolonies.api.colony.buildings.modules.AbstractBuildingModuleView;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventoryModuleView extends AbstractBuildingModuleView {
+
+    List<BlockPos> chests = new ArrayList<>();
+    List<BlockPos> tanks = new ArrayList<>();
+
+    @Override
+    public void deserialize(@NotNull FriendlyByteBuf buf) {
+        int chestSize = buf.readInt();
+        for (int i = 0; i < chestSize; i++) {
+            chests.add(buf.readBlockPos());
+        }
+
+        int tankSize = buf.readInt();
+        for (int i = 0; i < tankSize; i++) {
+            tanks.add(buf.readBlockPos());
+        }
+    }
+
+    @Override
+    public BOWindow getWindow() {
+        return null;
+    }
+
+    @Override
+    public String getIcon() {
+        return "";
+    }
+
+    @Override
+    public String getDesc() {
+        return "";
+    }
+
+    public List<BlockPos> getTanks() {
+        return tanks;
+    }
+
+    public List<BlockPos> getChests() {
+        return chests;
+    }
+}
